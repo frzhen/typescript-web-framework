98811f5316aa59161e327d09fa74679b1b80e249
Fred Zhen <fred.zhen@gmail.com>
2022-12-10 22:04:28 +0800

    Added fetch inside User class and tested

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,12 @@
  * @Date: 2022/12/8 23:12
  * @Email: fred.zhen@gmail.com
  */
-import axios from 'axios'
+import { User } from './models/User';
 
-const post_query = { name: 'Fred Zhen', age: 45}
-axios.post('http://localhost:3001/users', post_query)
-  .then(response => console.log(`${response.status}: ${response.statusText}`));
+const user = new User({ id: 1});
+
+user.fetch();
+
+setTimeout(() => {
+  console.log(user)}
+, 1000);
